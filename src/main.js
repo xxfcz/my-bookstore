1b0ebd6b932dbea3c96ad1f6fd0b41121df528f0
Xiao Xuefeng <xxf_cz@163.com>
2018-05-23 22:28:40 +0800

    +) use dexie to access IndexedDb

--- src/main.js
+++ src/main.js
@@ -3,24 +3,44 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import VueIdb from "vue-idb"
+import Dexie from 'dexie'
 
-Vue.use(VueIdb)
-Vue.config.productionTip = false
+// let db = new Dexie('test')
+// db.version(1).stores(
+//   { tests: 'id, title, created_at, updated_at' },
+//   { posts: 'id, owner' }
+// );
+// db.open();
+// db.posts.put({id:2, owner: 'bbc'});
 
-const idb = new VueIdb({
-  version: 1,
-  database: 'test',
-  schemas: [
-    { tests: 'id, title, created_at, updated_at' },
-    { posts: 'id, owner' }
-  ]
+// Define your database
+//
+var db = new Dexie('FriendsAndPetsDB')
+
+db.version(1).stores({
+  friends: '++id,name,isCloseFriend',
+  pets: '++id,name,kind'
 })
+
+db.version(2).stores({ friends: '++id,name,shoeSize' })
+db.version(3).upgrade(function(t) {
+  return t.friends.toCollection().modify(function(friend) {
+    // Modify each friend:
+    if (typeof friend.shoeSize === 'undefined') {
+      friend.shoeSize = friend.isCloseFriend ? 37 : 39
+      delete friend.isCloseFriend
+    }
+  })
+})
+db.version(4).stores({
+  friends: '++id,name'
+})
+
+Vue.prototype.$db = db;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  idb, idb,
   router,
   components: { App },
   template: '<App/>'
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -6,8 +6,7 @@
       {{announcement}}
     </section>
     <section>
-      <book-list :books="latestUpdated" heading="最近更新"
-        @onBookSelect="onBookSelect($event)">
+      <book-list :books="latestUpdated" heading="最近更新" @onBookSelect="onBookSelect($event)">
       </book-list>
     </section>
     <section>
@@ -21,35 +20,64 @@
 </template>
 
 <script>
-import axios from "axios"
-import BookList from "@/components/BookList.vue"
-import ModalDialog from "@/components/Dialog.vue"
+import axios from 'axios'
+import BookList from '@/components/BookList.vue'
+import ModalDialog from '@/components/Dialog.vue'
 
 export default {
   data() {
     return {
-      announcement: "",
+      announcement: '',
       slides: [],
       latestUpdated: [],
       recommended: []
-    };
+    }
   },
   methods: {
-    onBookSelect(book){
-      console.dir(book);
+    onBookSelect(book) {
+      console.dir(book)
       this.$refs.dialog.open()
     }
   },
-  components: {BookList, ModalDialog},
-  created(){
-    axios.get('/api/home').then(res => {
-      this.announcement = res.data.announcement;
-      this.slides = res.data.slides;
-      this.latestUpdated = res.data.latestUpdated;
-      this.recommended = res.data.recommended;
-    }, err=>{
-      console.log(err);
-    });
+  components: { BookList, ModalDialog },
+  created() {
+    console.log(this.$db)
+    //this.$db.version(2).stores({ posts: 'id, owner, count' });
+    // this.$db.posts.put({id:2, owner:'zzz'}).catch(function(err){
+    //   console.warn(err);
+    // });
+
+    //
+    // Put some data into it
+    //
+    this.$db.friends
+      .put({
+        name: 'bbc',
+        shoeSize: 45
+      })
+      .then(key => {
+        console.log('returns: ' + key)
+        return this.$db.friends.where({ name: 'bbc' }).toArray()
+      })
+      .then(friends=> {
+        console.log(friends)
+        if (friends) alert('how many? ' + friends.length)
+      })
+      .catch(function(error) {
+        alert('Ooops: ' + error)
+      })
+
+    axios.get('/api/home').then(
+      res => {
+        this.announcement = res.data.announcement
+        this.slides = res.data.slides
+        this.latestUpdated = res.data.latestUpdated
+        this.recommended = res.data.recommended
+      },
+      err => {
+        console.log(err)
+      }
+    )
   }
-};
+}
 </script>
