d2bf0122d5209292ef7922ad285d6397b4f9e151
Xiao Xuefeng <xxf_cz@163.com>
2018-05-24 16:08:38 +0800

    testing props of a component

--- test/unit/specs/MyComponent.spec.js
+++ test/unit/specs/MyComponent.spec.js
@@ -2,7 +2,7 @@ import Vue from 'vue/dist/vue'
 import MyComponent from '@/components/MyComponent'
 
 describe('MyComponent', () => {
-  it('$mount()', () => {
+  it('extend', () => {
     const msg = '你好'
     let Constructor = Vue.extend(MyComponent)
     const vm = new Constructor({
@@ -12,5 +12,17 @@ describe('MyComponent', () => {
     }).$mount()
     console.log('MyComponent', vm.$el)
     expect(vm.$el.textContent).to.eql(msg)
+  })
+})
+
+describe('another way', done => {
+  it('msg', () => {
+    const msg = '你们好'
+    const vm = new Vue(MyComponent).$mount()
+    vm.msg = msg
+    Vue.nextTick(() => {
+      expect(vm.$el.textContent).to.eql(msg)
+      done()
+    })
   })
 })
